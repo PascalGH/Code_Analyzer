8ce838fa1f2bca3dee789452ee1c735c4fb240b6
PascalGH <pascaldutrieux@gmail.com>
2019-01-09 15:01:44 +0100

    Test commit from PC

--- Analyser.py
+++ Analyser.py
@@ -1,15 +1,30 @@
-# This is the main repository to experiment code analysis
-# Another comment
-# More comment
+# This is the main file to experiment code analysis
 
-def one_function():
-    print('Ok2')
+# Configuration
+import Keywords_Map
+import re
+
+mymap = Keywords_Map.keywords_map
+mykeys = (Keywords_Map.keywords)
 
 
-one_function()
-one_function()
+comment_indicator = '#!'
+eol_indicator = '\n'
 
-# Another call to the function
-one_function()
 
-print('Completed')
+f = open("Analyser.py","r")
+content = f.read().split(eol_indicator)
+
+for line in content:
+    print(line)
+    sub_line = line.split(comment_indicator)
+    if sub_line[0] != '':
+        term = sub_line[0].split()[0].lower()
+ #       print(term)
+        for i in mykeys:
+             if re.search(i,term):
+                mymap[i]()
+                break
+    else:
+            print(f'Comment: ',sub_line)
+f.close()
---
+++ Keywords_Map.py
@@ -0,0 +1,11 @@
+def for_fn():
+    print('for block')
+
+def if_fn():
+    print('if block')
+
+def while_fn():
+    print('while block')
+
+keywords_map = {'for':for_fn,'if':if_fn,'while':while_fn}
+keywords = keywords_map.keys()
